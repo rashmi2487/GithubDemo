75d764f9992dd77102788bc5ed71ee85440ad782
Rashmi Shetty <rashmirai241982@gmail.com>
2024-07-19 16:29:27 +0530

    Initial Commit

---
+++ GithubDemo/GithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoApp.swift
+//  GithubDemo
+//
+//  Created by Rashmi Shetty on 19/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
